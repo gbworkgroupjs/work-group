8c8362549697d6be094873137d72c79ecf9266ec
Olimpicano <olimpicano@mail.ru>
2017-05-23 15:15:19 +0500

    +pug

--- Soft-Tech/gulpfile.js
+++ Soft-Tech/gulpfile.js
@@ -2,13 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
+var pug = require('gulp-pug');
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'pug'], function() {
 
     browserSync.init({
         server: "./"
     });
-
+    gulp.watch('./*.pug', ['pug']);
     gulp.watch("sass/*.scss", ['sass']);
     gulp.watch("/*.html").on('change', browserSync.reload);
 });
@@ -19,5 +20,12 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
+
+gulp.task('pug', function() {
+  return gulp.src('*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+})
 
 gulp.task('default', ['serve']);
