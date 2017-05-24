0244b035c52c28e7b01cdaecb5dd8d19de8091b0
Olimpicano <olimpicano@mail.ru>
2017-05-24 11:41:17 +0500

    + minify css

--- Soft-Tech/gulpfile.js
+++ Soft-Tech/gulpfile.js
@@ -3,14 +3,18 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 var pug = require('gulp-pug');
+var concat = require('gulp-concat');
+var minifyCss = require('gulp-clean-css');
+var csso = require('gulp-csso');
 
-gulp.task('serve', ['sass', 'pug'], function() {
+gulp.task('serve', ['sass', 'pug', 'concat'], function() {
 
     browserSync.init({
         server: "./"
     });
     gulp.watch('./*.pug', ['pug']);
     gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch('css/*.css', ['concat']);
     gulp.watch("/*.html").on('change', browserSync.reload);
 });
 
@@ -26,6 +30,15 @@ gulp.task('pug', function() {
     .pipe(pug())
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
+})
+
+gulp.task('concat', function() {
+    return gulp.src('css/*.css')
+            .pipe(concat('style.min.css'))
+            .pipe(minifyCss())
+            .pipe(gulp.dest('tmp'))
+            .pipe(browserSync.stream());
+       
 })
 
 gulp.task('default', ['serve']);
